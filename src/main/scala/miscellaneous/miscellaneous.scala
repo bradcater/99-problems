be9165f8b2ef3581ec1381c3d47ee438a7121a79
Brad Cater <bradcater@bradcater-Aspire-5750>
2015-01-02 11:36:00 -0500

    Substantial performance improvements for P90 (the 8-queens problem).

--- src/main/scala/miscellaneous/miscellaneous.scala
+++ src/main/scala/miscellaneous/miscellaneous.scala
@@ -7,22 +7,16 @@ package miscellaneous {
     // This is a classical problem in computer science. The objective is to place eight queens on a chessboard so that no two queens are attacking each other; i.e., no two queens are in the same row, the same column, or on the same diagonal.
     // Hint: Represent the positions of the queens as a list of numbers 1..N. Example: List(4, 2, 7, 3, 6, 8, 5, 1) means that the queen in the first column is in row 4, the queen in the second column is in row 2, etc. Use the generate-and-test paradigm.
     def place : List[List[Int]] = {
-      def reverse[@specialized(Int) T](l: List[T]) : List[T] = {
-        @tailrec def loop(acc: List[T], lst: List[T]) : List[T] = lst match {
-          case Nil => acc
-          case _   => loop(lst.head :: acc, lst.tail)
-        }
-        loop(Nil, l)
+      def mapToBoardIndices(l: List[Int]) : Set[Int] = {
+        Set(
+          l(0),      l(1) + 8,
+          l(2) + 16, l(3) + 24,
+          l(4) + 32, l(5) + 40,
+          l(6) + 48, l(7) + 56
+        )
       }
-      def mapToBoardIndices(l: List[Int]) : List[Int] = {
-        @tailrec def loop(acc: List[Int], lst: List[Int]) : List[Int] = {
-          if (lst.size == 0) acc
-          else loop(8 * acc.size + lst.head :: acc, lst.tail)
-        }
-        reverse(loop(Nil, l))
-      }
-      def checkLists(l: List[Int], groups: List[List[Int]]) : Boolean = {
-        groups.filter{lst => mapToBoardIndices(l).toSet.intersect(lst.toSet).size > 1}.size == 0
+      def noOverlapExists(s: Set[Int], groups: List[Set[Int]]) : Boolean = {
+        !groups.exists{st => s.intersect(st).size > 1}
       }
       //  0  1  2  3  4  5  6  7
       //  8  9 10 11 12 13 14 15
@@ -32,44 +26,51 @@ package miscellaneous {
       // 40 41 42 43 44 45 46 47
       // 48 49 50 51 52 53 54 55
       // 56 57 58 59 60 61 62 63
-      def validCols(l: List[Int]) : Boolean = {
-        checkLists(l, List(
-          List( 0,  8, 16, 24, 32, 40, 48, 56),  List( 1,  9, 17, 25, 33, 41, 49, 57), 
-          List( 2, 10, 18, 26, 34, 42, 50, 58),  List( 3, 11, 19, 27, 35, 43, 51, 59), 
-          List( 4, 12, 20, 28, 36, 44, 52, 60),  List( 5, 13, 21, 29, 37, 45, 54, 62), 
-          List( 6, 14, 22, 30, 38, 46, 54, 62),  List( 7, 15, 23, 31, 39, 47, 55, 63)
-        ))
+      val colSets = List(
+        Set( 0,  8, 16, 24, 32, 40, 48, 56),  Set( 1,  9, 17, 25, 33, 41, 49, 57), 
+        Set( 2, 10, 18, 26, 34, 42, 50, 58),  Set( 3, 11, 19, 27, 35, 43, 51, 59), 
+        Set( 4, 12, 20, 28, 36, 44, 52, 60),  Set( 5, 13, 21, 29, 37, 45, 53, 61), 
+        Set( 6, 14, 22, 30, 38, 46, 54, 62),  Set( 7, 15, 23, 31, 39, 47, 55, 63)
+      )
+      def validCols(s: Set[Int]) : Boolean = {
+        noOverlapExists(s, colSets)
       }
-      def validRows(l: List[Int]) : Boolean = {
-        checkLists(l, List(
-          List( 0,  1,  2,  3,  4,  5,  6,  7),  List( 8,  9, 10, 11, 12, 13, 14, 15), 
-          List(16, 17, 18, 19, 20, 21, 22, 23),  List(24, 25, 26, 27, 28, 29, 30, 31), 
-          List(32, 33, 34, 35, 36, 37, 38, 39),  List(40, 41, 42, 43, 44, 45, 46, 47), 
-          List(48, 49, 50, 51, 52, 53, 54, 55),  List(56, 57, 58, 59, 60, 61, 62, 63)
-        ))
+      val rowSets = List(
+        Set( 0,  1,  2,  3,  4,  5,  6,  7),  Set( 8,  9, 10, 11, 12, 13, 14, 15), 
+        Set(16, 17, 18, 19, 20, 21, 22, 23),  Set(24, 25, 26, 27, 28, 29, 30, 31), 
+        Set(32, 33, 34, 35, 36, 37, 38, 39),  Set(40, 41, 42, 43, 44, 45, 46, 47), 
+        Set(48, 49, 50, 51, 52, 53, 54, 55),  Set(56, 57, 58, 59, 60, 61, 62, 63)
+      )
+      def validRows(s: Set[Int]) : Boolean = {
+        noOverlapExists(s, rowSets)
       }
-      def validDiagonals(l: List[Int]) : Boolean = {
-        checkLists(l, List(
-          List( 0,  9, 18, 27, 36, 45, 54, 63), List( 1, 10, 19, 28, 37, 46, 56),
-          List( 2, 11, 20, 29, 38, 47, 57),     List( 3, 12, 21, 30, 39),
-          List( 4, 13, 22, 31),                 List( 5, 14, 23),
-          List( 6, 15),
-          List( 8, 17, 26, 35, 44, 53, 62),     List(16, 25, 34, 43, 52, 61),
-          List(24, 33, 42, 51, 60),             List(32, 41, 50, 59),
-          List(40, 49, 58),                     List(48, 57)
-        )) && checkLists(l, List(
-          List( 7, 14, 21, 28, 35, 42, 49, 56), List( 6, 13, 20, 27, 34, 41, 48),
-          List( 5, 12, 19, 26, 33, 40),         List( 4, 11, 18, 25, 32),
-          List( 3, 10, 17, 24),                 List( 2,  9, 16),
-          List( 1,  8),
-          List(15, 22, 29, 36, 43, 50, 57),     List(23, 30, 37, 44, 51, 58),
-          List(31, 38, 45, 52, 59),             List(39, 46, 53, 60),
-          List(47, 54, 61),                     List(55, 62)
-        ))
+      val rightDiagonalSets = List(
+        Set( 0,  9, 18, 27, 36, 45, 54, 63), Set( 1, 10, 19, 28, 37, 46, 55),
+        Set( 2, 11, 20, 29, 38, 47),         Set( 3, 12, 21, 30, 39),
+        Set( 4, 13, 22, 31),                 Set( 5, 14, 23),
+        Set( 6, 15),
+        Set( 8, 17, 26, 35, 44, 53, 62),     Set(16, 25, 34, 43, 52, 61),
+        Set(24, 33, 42, 51, 60),             Set(32, 41, 50, 59),
+        Set(40, 49, 58),                     Set(48, 57)
+      )
+      val leftDiagonalSets = List(
+        Set( 7, 14, 21, 28, 35, 42, 49, 56), Set( 6, 13, 20, 27, 34, 41, 48),
+        Set( 5, 12, 19, 26, 33, 40),         Set( 4, 11, 18, 25, 32),
+        Set( 3, 10, 17, 24),                 Set( 2,  9, 16),
+        Set( 1,  8),
+        Set(15, 22, 29, 36, 43, 50, 57),     Set(23, 30, 37, 44, 51, 58),
+        Set(31, 38, 45, 52, 59),             Set(39, 46, 53, 60),
+        Set(47, 54, 61),                     Set(55, 62)
+      )
+      def validDiagonals(s: Set[Int]) : Boolean = {
+        noOverlapExists(s, rightDiagonalSets) && noOverlapExists(s, leftDiagonalSets)
       }
       def validBoard(l: List[Int]) : Boolean = {
-        validCols(l) && validRows(l) && validDiagonals(l)
+        val s = mapToBoardIndices(l)
+        validCols(s) && validRows(s) && validDiagonals(s)
       }
+      // The entry in the ith position is the row index of the queen in the ith
+      // column.
       List(0, 1, 2, 3, 4, 5, 6, 7).permutations.toList.filter{validBoard(_)}
     }
   }
--- src/test/scala/miscellaneousSpec.scala
+++ src/test/scala/miscellaneousSpec.scala
@@ -4,18 +4,21 @@ import miscellaneous._
 
 class MiscellaneousSpec extends FlatSpec with Matchers {
   "P90" should "place 8 queens" in {
-    //println((new S99M()).place(0))
-    //List(0,3,6,2,5,1,7,4)
+    val solutions = (new S99M()).place
+    solutions.size should be (92)
+    solutions.toSet.intersect(Set(List(0,4,7,5,2,6,1,3))).size should be (1)
+    //println(solutions(0))
+    //println(solutions.size)
+    //List(0, 4, 7, 5, 2, 6, 1, 3)
     //   0 1 2 3 4 5 6 7
     // 0 x
-    // 1       x
-    // 2             x
-    // 3     x
-    // 4           x
-    // 5   x
-    // 6               x
-    // 7         x
-    (new S99M()).place.toSet.intersect(List(List(0,3,6,2,5,1,7,4)).toSet).size should be (1)
+    // 1             x
+    // 2         x
+    // 3               x
+    // 4   x
+    // 5       x
+    // 6           x
+    // 7    x
   }
 }
 
